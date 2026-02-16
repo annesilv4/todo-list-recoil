7814863438f14f4c58d5f5495dfc18e0ecc6cf1d
annesilv4 <carolaynebarbozasilva@gmail.com>
2026-02-16 18:14:34 -0300

    feat: melhorar visibilidade com estilos do TaskList

--- src/components/taskList.jsx
+++ src/components/taskList.jsx
@@ -1,12 +1,13 @@
 import { useRecoilValue } from "recoil";
 import { filterTaskSelector } from "../selectors/filterTaskSelector";
 import { TaskItem } from "./taskItem";
+import "./taskList.css";
 
 export function TaskList() {
     const tasks = useRecoilValue(filterTaskSelector);
 
     return (
-        <ul>
+        <ul className="task-list">
             {tasks.map(task => (
                 <TaskItem key={task.id} task={task} />
             ))}
