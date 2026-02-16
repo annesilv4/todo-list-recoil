e82a2e851897f933710e3457d97c85171ef11cbb
annesilv4 <carolaynebarbozasilva@gmail.com>
2026-02-16 11:53:05 -0300

    feat: add TaskList, TaskForm, and FilterTasks components to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './global.css'
+import { TaskList } from './components/taskList';
+import { TaskForm } from './components/taskForm';
+import { FilterTasks } from './components/filter';
 
 function App() {
 
   return (
-    <>
-      <h1>Todo List</h1>
-    </>
+    <div>
+      <h1>Todo-List Recoil</h1>
+      <TaskForm />
+      <FilterTasks />
+      <TaskList />
+    </div>
   )
 }
 
